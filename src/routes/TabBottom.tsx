87006b2def4e65d27358b48e603c3ca6e74a4dc9
Berwanger29 <viniciusberwangers@gmail.com>
2023-02-27 16:30:35 -0400

    Bottom tab styled

--- src/routes/TabBottom.tsx
+++ src/routes/TabBottom.tsx
@@ -1,7 +1,10 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { Ionicons } from '@expo/vector-icons';
+
 import { Home } from '../screens/Home';
 import { Search } from '../screens/Search';
+import theme from '../theme/theme';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,16 +13,46 @@ export function TabBottom() {
         <Tab.Navigator
             initialRouteName='Home'
             screenOptions={{
-                headerShown: false
+                headerShown: false,
+                tabBarLabelPosition: 'beside-icon',
+                tabBarActiveTintColor: theme.colors.white,
+                tabBarInactiveTintColor: theme.colors.gray500,
+                tabBarStyle: {
+                    backgroundColor: theme.colors.dark,
+                    borderTopWidth: 0
+                },
+                tabBarLabelStyle: {
+                    fontFamily: theme.fonts.semiBold,
+                    fontSize: 16
+                }
             }}
+
         >
             <Tab.Screen
                 name="Home"
                 component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name="home-outline"
+                            color={focused ? theme.colors.white : theme.colors.gray500}
+                            size={28}
+                        />
+                    )
+                }}
             />
             <Tab.Screen
                 name="Search"
                 component={Search}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name="search"
+                            color={focused ? theme.colors.white : theme.colors.gray500}
+                            size={28}
+                        />
+                    )
+                }}
             />
         </Tab.Navigator>
     );
